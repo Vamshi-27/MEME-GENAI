d63278562d780bc04d113d15d7568161071aa67e
Vamshi-27 <vamshira18@outlook.com>
2025-02-23 22:46:43 +0530

    commit

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Define dataset paths
-images_folder = r"C:\Users\91807\OneDrive\Desktop\MEME\memes900k\images"
-captions_path = r"C:\Users\91807\OneDrive\Desktop\MEME\memes900k\captions.txt"
+images_folder = r"C:\Users\Vamshi R A\Downloads\memes900k\memes900k\images"
+captions_path = r"C:\Users\Vamshi R A\Downloads\memes900k\memes900k\captions.txt"
 font_path = "impact.ttf"  # Ensure you have "impact.ttf" in your project directory
 
 # Cache model loading for efficiency
